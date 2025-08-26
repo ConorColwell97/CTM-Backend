5d25fbea0e9554284298f859c8d7a8499c61c7ec
ConorColwell97 <conormcolwell@gmail.com>
2025-08-26 03:11:45 +0100

    Database configuration set up

--- db.js
+++ db.js
@@ -8,11 +8,57 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-connection.connect(function(err) {
-    if(err) {
+connection.connect(function (err) {
+    if (err) {
         throw err;
     }
     console.log("Connected!");
 });
+
+
+connection.query(`CREATE TABLE IF NOT EXISTS Therapists (
+            Name varchar(30) PRIMARY KEY,
+            Title varchar(30) NOT NULL,
+            Email varchar(50) NOT NULL,
+            Location varchar(30) NOT NULL,
+            YearsOfPractice int NOT NULL,
+            Availability varchar(20) NOT NULL
+        )`,(err, result) => {
+            if(err) {
+                throw err;
+            }
+        });
+
+
+
+connection.query(`CREATE TABLE IF NOT EXISTS Clients (
+            Name varchar(30) PRIMARY KEY,
+            Email varchar(50) NOT NULL,
+            Location varchar(30) NOT NULL,
+            PhoneNumber varchar(10) NOT NULL,
+            Regularity varchar(10) NOT NULL
+        )`,(err, result) => {
+            if(err) {
+                throw err;
+            }
+        });
+
+
+
+connection.query(`CREATE TABLE IF NOT EXISTS Sessions (
+            Therapist varchar(30) NOT NULL,
+            Client varchar(30) NOT NULL,
+            Notes varchar(255) NOT NULL,
+            SessionDate varchar(10) NOT NULL,
+            Length int NOT NULL,
+            CONSTRAINT FK_Therapist FOREIGN KEY(Therapist) REFERENCES Therapists(Name) 
+            ON DELETE CASCADE ON UPDATE CASCADE,
+            CONSTRAINT FK_Client FOREIGN KEY(Client) REFERENCES Clients(Name) 
+            ON DELETE CASCADE ON UPDATE CASCADE
+        )`,(err, result) => {
+            if(err) {
+                throw err;
+            }
+        });
 
 export default connection; 
