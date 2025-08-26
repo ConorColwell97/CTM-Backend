19b7003717d803918d134f35c3c199ebeab43aec
ConorColwell97 <conormcolwell@gmail.com>
2025-08-26 03:51:23 +0100

    Changed port to dynamic port

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ server.use("/therapists", therapistRoutes);
 server.use("/clients", clientRoutes);
 server.use("/sessions", sessionRoutes);
 
-server.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
