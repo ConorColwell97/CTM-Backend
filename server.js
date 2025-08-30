70176fbd84ac22c49b280aaa1a9db6a285a458c8
ConorColwell97 <conormcolwell@gmail.com>
2025-08-30 12:01:17 +0100

    Hosting database in different location

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.use("/therapists", therapistRoutes);
 server.use("/clients", clientRoutes);
 server.use("/sessions", sessionRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
