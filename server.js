b3ab2b06343bb7192b3c2b12ed505a26e2363695
ConorColwell97 <conormcolwell@gmail.com>
2025-09-01 14:22:18 +0100

    small change

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ server.use("/sessions", sessionRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
